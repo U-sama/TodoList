b41b3f9ae994b674f3f09678349f53cfad335b05
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-03-22 23:46:34 +0200

    Add project files.

---
+++ TodoList/Data/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Data.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ TodoList/Extentions/MigrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+namespace TodoApi.Extentions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using AppDbContext _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            _db.Database.Migrate();
+        }
+    }
+}
